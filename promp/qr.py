7cec10bdd72f73b195ba857c56fc0ddf7fdb3e32
adam69iiu <160419234+adam69iiu@users.noreply.github.com>
2024-02-19 19:56:39 +0300

    Add files via upload

---
+++ promp/qr.py
@@ -0,0 +1,21 @@
+import qrcode
+from PIL import Image
+
+
+data = "https://www.youtube.com/watch?v=wGRF3GQ4Wdk"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("qrcode.png")
